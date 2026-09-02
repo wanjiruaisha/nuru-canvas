c3f1c1baa26c871cc66b6f09889e6bf6ec2ed8d8
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-09-02 21:29:32 +0300

    build: configure production Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './photo_gallery/templates/**/*.html',
+    './photo_gallery/**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
